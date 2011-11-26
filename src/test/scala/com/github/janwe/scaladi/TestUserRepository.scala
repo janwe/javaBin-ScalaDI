a10eb102d81f24ab82c0e26357b2195b7a3f4dfd
Mikkel Byrsø Dan-Rognlie <mikkel@decorus.dk>
2011-11-27 00:48:50 +0100

    Added jackson and jerkson for JSON serialization and parsing

--- src/test/scala/com/github/janwe/scaladi/TestUserRepository.scala
+++ src/test/scala/com/github/janwe/scaladi/TestUserRepository.scala
@@ -1,8 +1,9 @@
 package com.github.janwe.scaladi
-
+import com.codahale.jerkson.Json._
 
 class TestUserRepository extends UserRepository {
   def getUsers = {
-    "{user: {uid: 'test1', name: 'Test 1'}, user: {uid: 'test2', name: 'Test 2'}}"
+    val users = List(User("test1", "Test 1"), User("test2", "Test 2"))
+    generate(users)
   }
 }
--- src/test/scala/com/github/janwe/scaladi/UserRepositoryTest.scala
+++ src/test/scala/com/github/janwe/scaladi/UserRepositoryTest.scala
@@ -8,12 +8,13 @@ class UserRepositoryTest extends FunSuite with ShouldMatchers {
   test("User repository should return real users") {
     val repo : UserRepository = new RealUserRepository
 
-    repo.getUsers should be ("{user: {uid: 'janwe', name: 'Jan Fredrik'}, user: {uid: 'mikkelbd', name: 'Mikkel'}}")
+    repo.getUsers should be ("""[{"uid":"janwe","name":"Jan Fredrik Wedén"},{"uid":"mikkelbd","name":"Mikkel Dan-Rognlie"}]""")
   }
 
   test("Test repository should return test users") {
     val testRepo : UserRepository = new TestUserRepository
 
-    testRepo.getUsers should be ("{user: {uid: 'test1', name: 'Test 1'}, user: {uid: 'test2', name: 'Test 2'}}")
+    testRepo.getUsers should be ("""[{"uid":"test1","name":"Test 1"},{"uid":"test2","name":"Test 2"}]""")
   }
+
 }
