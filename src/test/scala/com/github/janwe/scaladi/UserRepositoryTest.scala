2c765cd4e13513170215186fe92cb65946a5b728
Jan Fredrik Wedén <jfweden@gmail.com>
2011-11-26 22:47:20 +0100

    Pushing initial setup for workshop

---
+++ src/test/scala/com/github/janwe/scaladi/UserRepositoryTest.scala
@@ -0,0 +1,10 @@
+package com.github.janwe.scaladi
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+class UserRepositoryTest extends FunSuite with ShouldMatchers {
+
+  test("blah") {
+  }
+}
